dec42050de36f5ed1e5fd99a8ba6df3ac4c0bc7a
Ryan <Hoodrn03@gmail.com>
2021-07-04 15:12:16 +0100

    Added Consumption script
    I have allowed the inputted numbers to eat one another increasing in value.
    
    Next, I will need to test the program properly than add in some error handling methods.
    
    Following the testing I will need to comment the code properly.

--- Documents/Daily Programmer Challenge - 004/Daily Programmer Challenge - 004/Daily Programmer Challenge - 004.cpp
+++ Documents/Daily Programmer Challenge - 004/Daily Programmer Challenge - 004/Daily Programmer Challenge - 004.cpp
@@ -5,8 +5,76 @@
 
 #pragma region Cannibal Number Calculator
 
-void l_CannibalNumbers(std::vector<std::string> numbers, int MaxNumber)
+void l_CannibalNumbers(std::vector<std::string> numbers, int maxNumber)
 {
+
+	unsigned int l_uiCurrentNumberPos = 0; 
+
+	bool l_bLoop = true;
+
+	int l_iLoopCounter = 0;
+
+	do
+	{
+		std::stringstream l_ssIntConverter;
+
+		int l_iCurrentNumber;
+
+		l_ssIntConverter << numbers[l_uiCurrentNumberPos];
+
+		l_ssIntConverter >> l_iCurrentNumber;
+
+		l_iLoopCounter++; 
+
+		for (unsigned int i = 0; i < numbers.size(); i++)
+		{
+			if (i != l_uiCurrentNumberPos)
+			{
+				std::stringstream l_ssConsumerConverter; 
+
+				int l_iNumberToConsume;
+
+				l_ssConsumerConverter << numbers[i];
+				l_ssConsumerConverter >> l_iNumberToConsume; 
+
+				if (l_iCurrentNumber > l_iNumberToConsume && l_iCurrentNumber < maxNumber)
+				{
+					std::cout << "Number " + std::to_string(l_iCurrentNumber) + " is consuming number " + std::to_string(l_iNumberToConsume) + " becoming " + std::to_string(l_iCurrentNumber + 1) << std::endl;
+
+					l_iCurrentNumber++; 
+
+					numbers[l_uiCurrentNumberPos] = std::to_string(l_iCurrentNumber); 
+
+					numbers.erase(numbers.begin() + i); 
+
+					numbers.shrink_to_fit(); 
+				}
+			}
+		}
+
+		if (l_iCurrentNumber >= maxNumber || l_iLoopCounter >= 5)
+		{
+			l_uiCurrentNumberPos++;
+
+			l_iLoopCounter = 0; 
+		}
+
+		if (l_uiCurrentNumberPos > numbers.size())
+		{
+			l_bLoop = false; 
+		}
+
+	} while (l_bLoop);
+
+
+	std::cout << "The Remaning numbers are : "; 
+
+	for (int j = 0; j < numbers.size(); j++)
+	{
+		std::cout << numbers[j] + " "; 
+	}
+
+	std::cout << std::endl;
 
 }
 
@@ -34,6 +102,14 @@ int main()
 	std::cout << "Every Time a number consumes another it will increase in value by 1 but they may only eat numbers of lower value than themselves. \n"; 
 
 	std::cin >> l_sInputTextMaxNumber; 
+
+	int l_iMaxNumber; 
+	std::stringstream l_ssIntConverter; 
+
+	l_ssIntConverter << l_sInputTextMaxNumber;
+	l_ssIntConverter >> l_iMaxNumber; 
+
+	l_CannibalNumbers(l_ManipulatedString, l_iMaxNumber); 
 
 }
 
